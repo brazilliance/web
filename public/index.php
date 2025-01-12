7c4ac8a3b75d977693366f32cd8fc4d85427f4af
Miquel Brazil <miquel@brazilliance.co>
2025-01-12 15:19:20 -0500

    feat: Enhanced HTML structure and styling
    The HTML file has been updated to improve its overall structure and presentation. Meta tags for character encoding and viewport have been added for better browser compatibility. The Tailwind CSS library has also been linked via a CDN for enhanced styling capabilities. Additionally, the main heading now includes classes for improved visual appeal.

--- public/index.php
+++ public/index.php
@@ -1,7 +1,14 @@
 <!DOCTYPE html>
+
 <head>
 	<title>Brazilliance</title>
+
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="https://cdn.tailwindcss.com"></script>
 </head>
+
 <body>
-	<h1>Brazilliance</h1>
+	<h1 class="text-3xl underline">Brazilliance</h1>
+</body>
 <html>
